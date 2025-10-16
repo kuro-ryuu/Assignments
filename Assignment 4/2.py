5f19eb6810fa06928d9d864048c93992850d7243
kuro-ryuu <91131557+kuro-ryuu@users.noreply.github.com>
2025-10-16 23:52:32 +0700

    Update 2.py

--- Assignment 4/2.py
+++ Assignment 4/2.py
@@ -1,6 +1,7 @@
 inch = int(input("Please enter the length in inch: "))
-while inch >= 0:
+brute_force_quit_thing = 0
+while inch >= 0 and brute_force_quit_thing == 0:
         print(f"{inch} inch is equal to {inch*2.54} centimeter.")
+        brute_force_quit_thing += 1
         if inch < 0:
             break
-
