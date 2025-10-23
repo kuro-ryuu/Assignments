84a960dc0c1e89125e97d0500ad6725dae40080d
cdkr <hito95979@gmail.com>
2025-10-23 22:50:30 +0700

    remove spaces

--- Assignment 5/1.py
+++ Assignment 5/1.py
@@ -1,11 +1,7 @@
 import random
-
-
 dice = int(input("How many dices would you like to use?: "))
 sum = 0
-
 for x in range(dice):
     roll = random.randint(1,6)
     sum += roll
-
 print(f"The sum of all rolls is {sum}")
